56f6f6678d7f670d2417a943466cc46357c7e8b6
iuliiabaranovska <iuliia.baranovska@gmail.com>
2016-05-12 17:45:29 +0300

    share logic was added

--- scripts/index/indexController.js
+++ scripts/index/indexController.js
@@ -10,7 +10,20 @@ GeekBooster.IndexController = (function() {
                 .on("click", function() {
                     GeekBooster.Share.facebook('URL', 'TITLE', 'IMG_PATH', 'DESC');
                 });
+
+            GeekBooster
+                .IndexView
+                .twshare()
+                .on("click", function() {
+                    GeekBooster.Share.twitter('URL', 'TITLE');
+                });
+
+            GeekBooster
+                .IndexView
+                .ggshare()
+                .on("click", function() {
+                    GeekBooster.Share.google('URL');
+                });
         }
     };
-
 }());
--- scripts/index/indexView.js
+++ scripts/index/indexView.js
@@ -3,8 +3,29 @@ var GeekBooster = GeekBooster || {};
 GeekBooster.IndexView = (function() {
 
     return {
+    	slide: function() {
+            $('.slider')
+                .on('unslider.ready', function() {
+                    $(this).find('li').show().end().parent().addClass("slider-margin");
+                })
+                .unslider({
+                    //autoplay: true,
+                    arrows: false,
+                    infinite: true,
+                    delay: 4000
+                });
+        },
+
         fbshare: function() {
         	return $('#fbshare');
+        },
+
+        twshare:function (){
+        	return $('#twshare');
+        },
+
+        ggshare:function () {
+        	return $('#ggshare');
         }
     };
 }());
